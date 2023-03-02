b64528cfe4a5e46388530100ed7ee4ca58bfb501
REREU LEMEIN <95699811+KHALEDKHLYAN@users.noreply.github.com>
2023-03-02 22:42:28 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
+// Create a new Date object representing the current date and time.
 var today = new Date();
+
+// Extract the current day of the month (as a number) from the 'today' object.
 var day = today.getDate();
+
+// Define an array of weekday names.
 var daylist = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Frieday","Saturday","Sunday"];
+
+// Display a message in the console indicating today's weekday name.
 console.log("Today is : " + daylist[day] + ".");
+
+// Extract the current hour, minute, and second from the 'today' object.
 var hour = today.getHours();
 var min = today.getMinutes();
 var second = today.getSeconds();
+
+// Determine whether it is currently AM or PM, based on the current hour.
 var prepand = ( hour >= 12 )? "PM" : "AM";
+
+// Handle special cases of midnight and noon, where hour value is 0.
 if (hour===0 && prepand === ' PM '){
     if (minute===0 && second===0) {
         hour = 12
@@ -24,4 +37,6 @@ if (hour ===0 && prepand === ' AM'){
         prepand = ' AM';
     }
 }
+
+// Display the current time (hour, minute, second, and AM/PM) in the console.
 console.log("Current Time : "+hour +prepand + " : " + second);
